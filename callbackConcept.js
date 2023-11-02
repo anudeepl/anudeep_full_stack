6b189a019c655d2e48a62afabdf37b5990054e95
Anudeep Lakkoju <anudeep.ace@gmail.com>
2023-11-02 23:58:20 +0530

    Closure and Call Back Concepts

---
+++ callbackConcept.js
@@ -0,0 +1,28 @@
+/* 
+A callback function is one of the superpowers of JavaScript. 
+It is the way JavaScript passes a function into another function as an argument. 
+The callback function is called in the outer function to execute an action.
+
+Arguments in JavaScript are values that are passed to the parameters of a function. 
+They are accessible only in the function they are passed into when that function is called. 
+Think of parameters as variables that are used to hold values in a function.
+*/
+
+//For example: showing a pop-up message welcoming a user after the user inputs a username.
+
+/*
+
+function greetings(username) {
+  console.log('Welcome ' + username);
+}
+//the function declaration above is to be passed into the function below thereby making it a callback function.
+
+function saveUserName(callback) {
+  var name = prompt('Please enter your username:');
+  callback(username);
+}
+//the above function states the callback function as a parameter
+
+saveUserName(greetings);
+
+*/
--- closureConcept.js
+++ closureConcept.js
@@ -5,6 +5,18 @@ with references to its surrounding state (the lexical environment).
 -> In other words, a closure gives you access to an outer function's scope from an inner function.
 */
 
+// Closure Function
+
+function OuterFunction() {
+    var outerVariable = 100;
+    function InnerFunction() {
+        console.log(outerVariable);
+    }
+    return InnerFunction;
+}
+var innerFunc = OuterFunction();
+
+innerFunc(); // 100
 
 
 
