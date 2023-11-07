967295473880c8acc1131803e356bab1331482ae
Anudeep Lakkoju <anudeep.ace@gmail.com>
2023-11-07 23:30:41 +0530

    pertaining to forIn loop

--- forIn.js
+++ forIn.js
@@ -1,3 +1,7 @@
+/*
+The for in loop in JavaScript allows you to iterate over all property keys of an object.
+*/
+
 const person = {fname:"John", lname:"Doe", age:25}; 
 let txt = "";
 for (let x in person) {
